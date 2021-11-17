cb42500f9287dbe01093d9802dd57e18de84f13b
Peter Houghton <pete@investigatingsoftware.co.uk>
2021-11-17 08:45:49 +0000

    Add comments / explainer

--- myproj/tensorflow1.py
+++ myproj/tensorflow1.py
@@ -1,8 +1,13 @@
 import tensorflow as tf
 
+# There are different versions of tensorflow, 
+# This was executed with the direct ml on wsl2 version.
+
 x = tf.Variable(0.) 
 with tf.GradientTape() as tape: 
  y = 2* (3 * x + 3)
 grad_of_y_wrt_x = tape.gradient(y, x)
 
+# Included in the tensor (amongst the debug) will be the value of the gradient.
+# this has been calculated via a backpropagation algorithm.
 print(grad_of_y_wrt_x)
